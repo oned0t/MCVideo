17c55d8d0c52d0c6e592f5dcb808e80342028e4a
tazryder <tazryder@me.com>
2021-02-14 01:29:19 +0000

    finish

--- app/src/main/java/com/ictech/mcvideo/activity/SplashActivity.kt
+++ app/src/main/java/com/ictech/mcvideo/activity/SplashActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import com.ictech.mcvideo.sharedpref.AppPref
 import android.os.Bundle
 import android.os.Handler
+import android.view.Menu
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
@@ -17,8 +18,9 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.splash)
 
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
         // This is used to hide the status bar and make
         // the splash screen as a full screen activity.
         window.setFlags(
@@ -26,20 +28,12 @@ class SplashActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
-        // we used the postDelayed(Runnable, time) method
-        // to send a message with a delayed time.
-        Handler().postDelayed({
-            val intent = Intent(this, AppIntroActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 3000) // 3000 is the delayed time in milliseconds.
-
-        if (AppPref.isAppIntroShown) {
-            if (resources.getBoolean(R.bool.enable_mandatory_authentication)) {
+        if (!AppPref.isAppIntroShown) {
+            if (!resources.getBoolean(R.bool.enable_mandatory_authentication)) {
                 // Authentication is mandatory
-                if (FirebaseAuth.getInstance().currentUser != null) {
+                if (FirebaseAuth.getInstance().currentUser == null) {
                     // Intro shown and user authenticated
-                    MainActivity.startActivity(this)
+                    AppIntroActivity.startActivity(this)
                     finish()
                 } else {
                     // Intro shown but user unauthenticated
@@ -52,7 +46,7 @@ class SplashActivity : AppCompatActivity() {
                 finish()
             }
         } else {
-            AppIntroActivity.startActivity(this)
+            MainActivity.startActivity(this)
             finish()
         }
     }
